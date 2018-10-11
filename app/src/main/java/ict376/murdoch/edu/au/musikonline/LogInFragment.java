2a15e969f44014392a15fbefdc11c984448ecd40
NSnake151 <19525940+NSnake151@users.noreply.github.com>
2018-10-11 15:38:05 +0800

    Login Functionality Done
    Added multiple new classes to build the database functionality, and upgraded loginfragment to work as expected.

--- app/src/main/java/ict376/murdoch/edu/au/musikonline/LogInFragment.java
+++ app/src/main/java/ict376/murdoch/edu/au/musikonline/LogInFragment.java
@@ -1,6 +1,7 @@
 package ict376.murdoch.edu.au.musikonline;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
@@ -8,12 +9,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class LogInFragment extends Fragment {
 
     private Button mSubmitButton;
     private EditText mUsername;
     private EditText mPassword;
+    private DataManager mDataManager;
+    private List<Customer> mCustomerArrayList;
+    boolean validLogin = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -26,17 +35,37 @@ public class LogInFragment extends Fragment {
     {
         View v = inflater.inflate(R.layout.loginfragment, container, false);
 
+        mUsername = (EditText) v.findViewById(R.id.usernameField);
+        mPassword = (EditText) v.findViewById(R.id.passwordField);
+
+        mDataManager = new DataManager(getContext());
+
         mSubmitButton = (Button) v.findViewById(R.id.loginButton);
         mSubmitButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v)
             {
+                String username =  mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+                mCustomerArrayList = mDataManager.getCustomers();
+
+                for(int i = 0; i < mCustomerArrayList.size(); i++)
+                {
+                    if(mCustomerArrayList.get(i).getCust_username().equals(username) &&
+                            mCustomerArrayList.get(i).getCust_password().equals(username))
+                    {
+                        validLogin = true;
+                    }
+                }
+
+
                 //if login details are valid
                 //replace with a function that checks a database and returns a boolean
-                if(true)
+                if(validLogin)
                 {
-                    Fragment libraryFragment = new PlayingFragment();
+                    Toast.makeText(getActivity().getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                    Fragment libraryFragment = new LibraryFragment();
                     FragmentTransaction ft = getFragmentManager().beginTransaction();
                     ft.replace(R.id.fragement_container, libraryFragment);
                     ft.commit();
@@ -44,6 +73,7 @@ public class LogInFragment extends Fragment {
                 else
                 {
                     //makeToast w/errorMessage
+                    Toast.makeText(getActivity().getApplicationContext(), "Login Information Invalid", Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- app/src/main/java/ict376/murdoch/edu/au/musikonline/MainActivity.java
+++ app/src/main/java/ict376/murdoch/edu/au/musikonline/MainActivity.java
@@ -1,5 +1,8 @@
 package ict376.murdoch.edu.au.musikonline;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +10,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private DataManager mDataManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
         Fragment fragment = fm.findFragmentById((R.id.fragement_container));
 
+        mDataManager = new DataManager(getApplicationContext());
+
         if(fragment == null)
         {
             fragment = new LogInFragment();
             fm.beginTransaction().add(R.id.fragement_container, fragment).commit();
         }
     }
+
+
 }
